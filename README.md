54c7e2bdceb8aeba199a7db7dbec8c875b10f472
Lokesh <appsbotta@gmail.com>
2025-02-28 22:33:04 +0530

    logging and exception

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # End to End ML Project (student_performance)
 
 ## workflow
+1. created setup.py, template.py and requirements.txt
+
+2. Created custom exception in exception.py and logger.py
+
+## To run 
 1. Create a virtual env
 
 2. Install requirements.txt
--- template.py
+++ template.py
@@ -5,10 +5,31 @@ dirs = [
     'research',
     'model',
     'templates',
-    'artifacts'
+    'artifacts',
+    os.path.join('src','components'),
+    os.path.join('src','pipeline'),
 ]
 
 for dir in dirs:
     os.makedirs(dir,exist_ok=True)
     with open(os.path.join(dir,'.gitkeep'),'w') as f:
         pass
+
+files = [
+    os.path.join('src','__init__.py'),
+    os.path.join('src','logger.py'),
+    os.path.join('src','exception.py'),
+    os.path.join('src','utils.py'),
+    os.path.join('src/components','__init__.py'),
+    os.path.join('src/components','data_ingestion.py'),
+    os.path.join('src/components','data_transforamtion.py'),
+    os.path.join('src/components','model_trainer.py'),
+    os.path.join('src/pipeline','__init__.py'),
+    os.path.join('src/pipeline','prediction_pipeline.py'),
+    os.path.join('src/pipeline','train_pipeline.py'),
+]
+
+for file in files:
+    if not os.path.exists(file):
+        with open(file,'w') as f:
+            pass
